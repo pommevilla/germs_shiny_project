ef6f34368a7c54edf64933ecd0d7a062127ff55c
pommevilla <pvillanueva13@gmail.com>
2022-02-17 09:56:07 -0600

    Sketching out project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# germs_shiny_project
+# Secret GERMS Shiny Project
+
+A secret project for Jia and Paul.
+
+## Draft outline:
+
+* Take in a data set
+	- [ ] CSV
+	- [ ] RDS
+* Clean it
+	- [ ] Decide what a "clean" data set looks like
+* Display an ordination
+	- [ ] Display NMDS
+	- [ ] Add options to switch between different ordinations (t-SNE, MDS, PCA, etc.)
+	- [ ] Add options to change labels (axes, title, caption, legend, etc.,)
+* Do some statistical tests
+	- [ ] Test for significance between variables between groups (select group and variable)
+* Downloads
+	- [ ] Parameterized RMarkdown files
---
+++ germs/app.R
@@ -0,0 +1,63 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(DT)
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    selectInput("v_species", "Flower Species",
+                               choices = iris %>% distinct(Species)),
+    
+      downloadButton("report", "Download Iris report", style = "width:100%;")
+    
+    # downloadHandler(
+    #   filename = function() {
+    #     paste0("Testing_IRIS", Sys.Date(), "csv")
+    #   },
+    #   content = function
+    # )
+    
+  ),
+  dashboardBody(fluidPage(DTOutput('tbl')))
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$tbl = renderDT(
+    iris %>% filter(Species %in% input$v_species), 
+    options = list(lengthChange = FALSE)
+    )
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("data-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(
+        iris %>% filter(Species %in% input$v_species), 
+        file
+      )
+    }
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+# shinyApp(
+#   ui = fluidPage(DTOutput('tbl')),
+#   server = function(input, output) {
+#     output$tbl = renderDT(
+#       iris, options = list(lengthChange = FALSE)
+#     )
+#   }
+# )
